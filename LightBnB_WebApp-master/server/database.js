930247f2829848392b4e39fe441c34164139a4ed
victorcwyu <victorcwyu@gmail.com>
2020-03-06 00:51:08 +0000

    Fixed bug which would not add new properties

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -107,10 +107,39 @@ exports.getAllProperties = getAllProperties;
 // Add a property to the database
 const addProperty = function(property) {
   return pool.query(`
-    INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, true)
+    INSERT INTO properties (
+      owner_id, 
+      title, 
+      description, 
+      thumbnail_photo_url, 
+      cover_photo_url, 
+      cost_per_night, 
+      parking_spaces, 
+      number_of_bathrooms, 
+      number_of_bedrooms, 
+      country, 
+      street, 
+      city, 
+      province, 
+      post_code)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
     RETURNING *;
-  `, [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms, property.country, property.street, property.city, property.province, property.post_code, property.active])
+  `, [
+    property.owner_id, 
+    property.title, 
+    property.description, 
+    property.thumbnail_photo_url, 
+    property.cover_photo_url, 
+    property.cost_per_night, 
+    property.parking_spaces, 
+    property.number_of_bathrooms, 
+    property.number_of_bedrooms, 
+    property.country, 
+    property.street, 
+    property.city, 
+    property.province, 
+    property.post_code, 
+    ])
     .then(res => res.rows[0]);
 };
 exports.addProperty = addProperty;
